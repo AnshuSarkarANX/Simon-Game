57013ed5912c4d223850f8a7fe66bc1c3323df36
AnshuSarkarANX <anshusarkaranx@gmail.com>
2022-04-27 23:11:17 +0530

    some changes

--- game.js
+++ game.js
@@ -1,10 +1,10 @@
 var gamePattern = [];
 var randomNumber;
-var blue_a = audio ("sounds\blue.mp3");
-var red_a = audio ("sounds\red.mp3");
-var green_a = audio ("sounds\green.mp3");
-var yellow_a = audio ("sounds\yellow.mp3");
-var wrong_a = audio ("sounds\wrong.mp3");
+var blue_a = new Audio("sounds/blue.mp3");
+var red_a = new Audio("sounds/red.mp3");
+var green_a = new Audio("sounds/green.mp3");
+var yellow_a = new Audio("sounds/yellow.mp3");
+var wrong_a = new Audio("sounds/wrong.mp3");
 var buttonColours = ['red','blue','green','yellow'];
  function nextSequence(){
    randomNumber = Math.floor(Math.random()*4);
@@ -13,10 +13,14 @@ var buttonColours = ['red','blue','green','yellow'];
  var randomChosenColour = buttonColours[randomNumber];
  gamePattern.push(randomChosenColour);
 function playgame(event){
-   console.log(event.key);
-  nextSequence();
-  randomChosenColour_a.play();
+console.log(event.key);
+nextSequence();
+randomChosenColour_a.play();
 $('#' + randomChosenColour).animate({opacity:0.5},50);
 $('#' + randomChosenColour).animate({opacity:0},50);
 $('#' + randomChosenColour).animate({opacity:1},50);
+}
+$(".btn").click( handler());
+function handler(evt){
+  console.log(evt);
 }
