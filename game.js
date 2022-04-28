e5d99e47f0e7421699fbb7a22c9a7226d067f2ce
AnshuSarkarANX <anshusarkaranx@gmail.com>
2022-04-29 01:14:41 +0530

    finally fixe dthose issues with some help

--- game.js
+++ game.js
@@ -1,26 +1,22 @@
 var gamePattern = [];
+var userClickedPattern =[];
 var randomNumber;
-var blue_a = new Audio("sounds/blue.mp3");
-var red_a = new Audio("sounds/red.mp3");
-var green_a = new Audio("sounds/green.mp3");
-var yellow_a = new Audio("sounds/yellow.mp3");
-var wrong_a = new Audio("sounds/wrong.mp3");
+var wrong = new Audio("sounds/wrong.mp3");
 var buttonColours = ['red','blue','green','yellow'];
  function nextSequence(){
    randomNumber = Math.floor(Math.random()*4);
+   var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
    console.log(randomNumber);
+   var color = new Audio("sounds/"+ randomChosenColour +".mp3");
+   color.play();
+   $('#' + randomChosenColour).animate({opacity:0.5},50);
+   $('#' + randomChosenColour).animate({opacity:0},50);
+   $('#' + randomChosenColour).animate({opacity:1},50);
  }
- var randomChosenColour = buttonColours[randomNumber];
- gamePattern.push(randomChosenColour);
-function playgame(event){
-console.log(event.key);
-nextSequence();
-randomChosenColour_a.play();
-$('#' + randomChosenColour).animate({opacity:0.5},50);
-$('#' + randomChosenColour).animate({opacity:0},50);
-$('#' + randomChosenColour).animate({opacity:1},50);
-}
-$(".btn").click( handler());
-function handler(evt){
-  console.log(evt);
-}
+$(".btn").click(function handler(){
+  var userChosenColour=$(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern);
+
+});
