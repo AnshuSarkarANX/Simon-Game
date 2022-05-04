5500c33a301f1630d519bb7fd9b0ee3ec2a48c15
AnshuSarkarANX <anshusarkaranx@gmail.com>
2022-05-04 23:54:56 +0530

    game logic first part done with the help of answer😥

--- game.js
+++ game.js
@@ -5,41 +5,68 @@ var level = 0;
 var started = false;
 var wrong = new Audio("sounds/wrong.mp3");
 var buttonColours = ['red','blue','green','yellow'];
+var i = 0;
+//starting game
+$(document).keypress(function(){
+  if(!started){
+    $("#level-title").text("Level "+ level);
+    nextSequence();
+    started = true;
+  }
+})
+// game sequence function
  function nextSequence(){
    level++;
+   userClickedPattern=[];
    $("#level-title").text("Level "+ level);
    randomNumber = Math.floor(Math.random()*4);
    var randomChosenColour = buttonColours[randomNumber];
    playsound(randomChosenColour);
     gamePattern.push(randomChosenColour);
-   console.log(randomNumber);
-   $('#' + randomChosenColour).animate({opacity:0.5},50);
-   $('#' + randomChosenColour).animate({opacity:0},50);
-   $('#' + randomChosenColour).animate({opacity:1},50);
+   animatePress(randomChosenColour);
  }
+ // click function
 $(".btn").click(function handler(){
   var userChosenColour=$(this).attr("id");
   playsound(userChosenColour);
   userClickedPattern.push(userChosenColour);
 animatePress(userChosenColour);
+checkAnswer(userClickedPattern.length-1);
 });
+/*game function
+  if (gamePattern=userClickedPattern) {
+    userClickedPattern = [0];
+    nextSequence();
+  } else {
+    $("#level-title").text("Game Over.");
+    i++;
+  }
+*/
+//answer
+function checkAnswer(currentLevel){
+  if (gamePattern[currentLevel]===userClickedPattern[currentLevel]){
+    console.log("success");
+    console.log(gamePattern);
+    console.log(userClickedPattern);
+    if(gamePattern.length===userClickedPattern.length){
+      setTimeout(function(){
+        nextSequence();
+      },1000);
+    }
+  }
+  else{
+    console.log("wrong")
+}
+}
+
+// different colours sound
 function playsound(name){
   var color = new Audio("sounds/"+ name +".mp3");
   color.play();
 }
+// button press animation
 function animatePress(currentColour){
-  $('#'+currentColour).addClass("pressed ");
-  setTimeout(function (){
-  $('#'+currentColour).removeClass("pressed");
-  }, 100);
+  $('#' + currentColour).animate({opacity:0.5},50);
+  $('#' + currentColour).animate({opacity:0},50);
+  $('#' + currentColour).animate({opacity:1},50);
 }
-//starting game
-$(document).keypress(function(){
-  if(!started){
-
-    $("#level-title").text("Level "+ level);
-    nextSequence();
-    started = true;
-
-  }
-})
